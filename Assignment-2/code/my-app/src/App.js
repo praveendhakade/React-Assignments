6bb015471ea94941e71aaf48cdc27f080d4d70bb
praveenDhakade <praveendhakade@gmail.com>
2022-02-02 13:11:01 +0530

    First commit - All tests passed.

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,8 +7,16 @@ function App(props) {
  const {employee}=props;
     
 return(
-  <div className="container">
+  <div className="container App">
   {/* Start your React code here */}
+  <img className="App-logo" src={employee.profileImg} alt='' />
+  <h1 className="profileName">{employee.name}</h1>
+  <h5 className="subHeading">Location</h5>
+  <h3 className='details'>{employee.location}</h3>
+  <h5 className="subHeading">Blood Group</h5>
+  <h3 className='details'>{employee.bloodGroup}</h3>
+  <h5 className="subHeading">Age</h5>
+  <h3 className='details'>{employee.age}</h3>
 </div>
 )
 }
