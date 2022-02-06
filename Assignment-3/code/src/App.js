da5e87d1af434d6678aebd70755b4caa3f66ddf8
praveenDhakade <praveendhakade@gmail.com>
2022-02-06 11:24:39 +0530

    Created object and Layout, All tests passed

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let arrow = "<";
+let arrowRight = ">";
+const employee = {
+  name:"Dante",
+  email:"dante@Inferno.com",
+  phone:478125462,
+  employeeId:"000124",
+  Appointment:"9:30:00",
+  profileImg: "ttps://www.w3schools.com/howto/img_avatar.png"
+}
 
-var arrow="<"
 function App() {
+  const {useState} = React;
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  
+  React.useEffect(()=> {const timerId = setInterval(()=>  setTime(new Date().toLocaleTimeString()),1000
+  );
+    return ()=>{
+      clearInterval(timerId);
+    }
+}, [])
+
+  
   return (
     <div className="site-container">
     {/* Start Your code here */}
+    <header className='page-header'> <div>
+      <button className='arrow'>{arrow}</button></div>
+      <div className='title'>
+      <h2>{employee.name}</h2>
+      <p>{employee.employeeId}</p>
+      </div>
+      <div> <button className='printBtn'>Print</button></div>
+    </header>
+    <div className='customer-info'>
+      <p><strong>Appointment On: </strong>{employee.Appointment}</p>
+      <p><strong>Email: </strong>{employee.email}</p>
+      <p><strong>Phone: </strong>{employee.phone}</p>
     </div>
+    <strong></strong>
+    <div className='order-info'>
+      <label><strong>Status</strong><p><li>In Progress</li></p></label>
+      <label><strong>Door</strong><p>Lucifer</p></label>
+      <label><strong>Time</strong><p>{time}</p></label>
+    </div>
+
+    <div className='product-list'>
+      <div className='img-check'>
+      <label>
+      <input type="checkbox"></input></label>
+      <img src="https://www.w3schools.com/howto/img_avatar.png" alt='' width='100' height="100" />
+      </div>
+      <div className='product-info'>
+        <h2>Ervin Howell</h2>
+        <p>quia et suscipit nsuscipit recusandae consequuntur expedita et cum nreprehenderit</p>
+      </div>
+      <div><button className='arrow'>{arrowRight}</button></div>
+    </div>
+
+    </div>
+
   );
 }
 
